656995bf771591590cda8ba2b3e3341cafdd354f
Gaurav <gaurav.shinde2@bajajfinserv.in>
2024-03-29 15:47:10 +0530

    listing call added

--- src/app/components/listing/listing.service.ts
+++ src/app/components/listing/listing.service.ts
@@ -1,9 +1,19 @@
+import { HttpBackend, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CommonConstants } from 'src/app/shared/constants/comman-constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListingService {
+  private httpClient: HttpClient;
 
-  constructor() { }
+  constructor(private handler: HttpBackend) {
+    this.httpClient = new HttpClient(handler);
+  }
+
+  public listingGet(appId: any){
+    const url = CommonConstants.APIURL.listingGet.replace('{applicationid}', appId);
+    return this.httpClient.get(url);
+  }
 }
